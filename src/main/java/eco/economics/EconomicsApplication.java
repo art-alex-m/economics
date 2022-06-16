298db8eff35f6e9a3f7209b3b6d7eaff59a04271
Alexander M Artamonov <art.alex.m@gmail.com>
2022-06-16 15:50:23 +0300

    Инициация проекта

---
+++ src/main/java/eco/economics/EconomicsApplication.java
@@ -0,0 +1,13 @@
+package eco.economics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eco/economics/EconomicsApplicationTests.java
@@ -0,0 +1,13 @@
+package eco.economics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconomicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
